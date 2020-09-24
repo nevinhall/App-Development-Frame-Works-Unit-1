059fe48d62570874e0cc33721fd55a4c4010dbf5
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-09-24 13:56:14 +0100

    print publisher and franchiser in main

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,13 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MainApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         County cork = new County("Cork");
         System.out.println(cork);
 
-        ApplicationContext context =  new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         County k = (County) context.getBean("kerry");
         System.out.println(k);
+
+        Franchise jl = (Franchise) context.getBean("Justice league");
+        System.out.println(jl);
+
+        Franchise aw = (Franchise) context.getBean("Arrowverse");
+        System.out.println(aw);
 
     }
 
